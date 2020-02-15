cb1d0931b7ba7911fd0c422b567dcdb4be545c64
jamesmblackii <61098009+jamesmblackii@users.noreply.github.com>
2020-02-15 18:22:41 -0500

    Add files via upload
    First Submission

---
+++ P2T1_JamesBlackII.py
@@ -0,0 +1,14 @@
+# Sales Prediction Tutorial
+# 2/15/2020
+# CTI-110 P2T1 - Sales Prediction
+# James Black II
+
+#Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+                               
